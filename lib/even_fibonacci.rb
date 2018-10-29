83766cb5e22a91d770daad5fe616667e291ef98b
john-raber <jrab224@gmail.com>
2018-10-29 14:42:03 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+
+def collect_terms(limit)
+  range = [1, 2]
+
+  while range[range.size - 1] + range[range.size - 2] <= limit
+    range << range[range.size - 1] + range[range.size - 2]
+  end
+
+  range.select do |n|
+    n % 2 == 0
+  end
+end
+
+def even_fibonacci_sum(limit)
+  collect_terms(limit).inject(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+require 'pry'
+
+class EvenFibonacci
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_terms
+    range = [1, 2]
+
+    while range[range.size - 1] + range[range.size - 2] <= @limit
+      range << range[range.size - 1] + range[range.size - 2]
+    end
+
+    range.select do |n|
+      n % 2 == 0
+    end
+  end
+
+  def sum
+    collect_terms.inject(:+)
+  end
+end
